0d5ce9b149375caf0c697b7147c45e714fdb873c
Azoooooooooz <44898336+epic3azoozez135@users.noreply.github.com>
2018-12-22 02:24:33 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -87,29 +87,6 @@ if (message.content.startsWith(adminprefix + 'sett')) {
 }
 });
 
-      client.on("message", message => {
-    var prefix = "s$";
- 
-            var args = message.content.substring(prefix.length).split(" ");
-            if (message.content.startsWith(prefix + "clear")) {
-   if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('⚠ | **لا يوجد لديك صلاحية لمسح الشات**');
-        var msg;
-        msg = parseInt();
-      
-      message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
-      message.channel.sendMessage("", {embed: {
-        title: "Done | تــم مسح الشات",
-        color: 0x06DF00,
-        description: "تم مسح الرسائل ",
-        footer: {
-          text: " Starex Protect. "
-        }
-      }}).then(msg => {msg.delete(3000)});
-                          }
-
-     
-})
-      
       
       
 client.login(process.env.BOT_TOKEN); 
